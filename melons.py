4d8d16a82eb1043daff8a3e063754d7cf75d2637
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-01 17:46:03 -0700

    Finish part 2

--- melons.py
+++ melons.py
@@ -15,6 +15,10 @@ class AbstractMelonOrder(object):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species == "Christmas":
+            base_price *= 1.5
+        
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -55,3 +59,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+
+        total = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty < 10:
+            total += 3
+        
+        return total
+
+
+
+# christmas_melon = AbstractMelonOrder("christmas", )
